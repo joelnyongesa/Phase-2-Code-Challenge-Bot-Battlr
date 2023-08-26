f7db066501377dffb04db62ce42076cfd35467c8
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-26 15:53:30 +0300

    feat: Accept the prop passed down from the App.js' handleAddBot, and use the prop to render the added props to the YourBotArmy component

--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,7 +1,32 @@
-const YourBotArmy = () => {
+const YourBotArmy = ({yourBots}) => {
+    /*
+        Add an individual bot to my army by clicking on it. The selected bot should render in this component, and the bot can only be enlisted ONCE. The bot does not disappear from the BotsCollection.
+
+        Create a state variable.
+        Add using the spread operator.
+
+    */
+    //Loop over the bots to return the bots in YourBotArmy.
+    const yourBotsList = yourBots.map((yourBot)=>{
+        return(
+            <div key={yourBot.id}>
+                <img src={yourBot.avatar_url} alt={yourBot.name}/>
+                <h3>{yourBot.name}</h3>
+                <p>Class: {yourBot.bot_class}</p>
+                <p>Catch phrase: {yourBot.catchphrase}</p>
+                <p>Created at: {yourBot.created_at}</p>
+                <p>Updated at: {yourBot.updated_at}</p>
+                Health: <progress value={yourBot.health} max='100'/>
+                Damage: <progress value={yourBot.damage} max='100'/>
+                Armor: <progress value={yourBot.armor} max='100'/>
+            </div>
+        )
+    })
+
     return ( 
         <div>
-            <h1>This is Your Bot Army Page</h1>
+            <h1>This is Your Bots List</h1>
+            {yourBotsList}
         </div>
      );
 }
