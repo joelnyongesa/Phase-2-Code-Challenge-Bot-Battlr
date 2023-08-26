2b9fd8822e8239eedf803f47226c87ca13b520da
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-26 13:58:16 +0300

    Project initialization

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-rxl font-bold underline'>Bot Battlr</h1>
+    </div>
+  );
+}
+
+export default App;
