e5011d529de721986a34124489b35c6334ee4fd1
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-26 14:12:49 +0300

    Add Landing Page component, nav bar and page heading

---
+++ src/components/LandingPage.js
@@ -0,0 +1,14 @@
+const LandingPage = () => {
+    return ( 
+        <div>
+            <nav>
+                <button>Home</button>
+                <button>Bot Collection</button>
+                <button>Your Bot Army</button>
+            </nav>
+            <h1>Welcome to <strong>Bot Battlr</strong>, the one and only spot in the known universe where you can custom build your own Bot Army!</h1>
+        </div>
+     );
+}
+ 
+export default LandingPage;
