019103419493d1f1ddffd33bed2e69b392e81214
joelnyongesa <joelnyongesa148@gmail.com>
2023-08-26 14:06:40 +0300

    Update App.js header

--- src/components/App.js
+++ src/components/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-rxl font-bold underline'>Bot Battlr</h1>
+      <h1>Welcome to <strong>Bot Battlr</strong>, the one and only spot in the known universe where you can custom build your own Bot Army!</h1>
     </div>
   );
 }
