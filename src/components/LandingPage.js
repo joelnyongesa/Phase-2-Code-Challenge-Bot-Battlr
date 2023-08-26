9bc3afe012277d4bfda4f68b3416c2b855975c19
Joel K Nyongesa <66782583+joelnyongesa@users.noreply.github.com>
2023-08-26 21:15:43 +0300

    Joel/styling (#6)
    * Add Landing Page component, nav bar and page heading
    
    * Add Landing Page component to the App.js component
    
    * Update the Browser Router from react-router-dom for routing
    
    * Added every page needed to make the landing page as outlets
    
    * Update routes in App.js
    
    * feat: Display all bots
    
    * feat: Add a function to handle click event when a user clicks on a bot (to add the bot to 'YourBots Collection'
    
    * feat: add event listener to each bot's container
    
    * feat: Accept the prop passed down from the App.js' handleAddBot, and use the prop to render the added props to the YourBotArmy component
    
    * feat: click on a bot in my collection to make it disappear from the YourBotArmy component, non persistent
    
    * feat: delete button to persistently delete the bots from the server
    
    * Apply styling on the bot cards, home and other pages in the app

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -3,11 +3,14 @@ import { Link, Outlet } from "react-router-dom";
 const LandingPage = () => {
     return ( 
         <div>
-            <nav>
-                <button><Link to='/home'>Home</Link></button>
-                <button><Link to='/bot-collection'>Bot Collection</Link></button>
-                <button><Link to='/your-bot-army'>Your Bot Army</Link></button>
-            </nav>
+            <div className="flex justify-between m-5">
+                <h1 className="w-1/2 text-5xl font-bold">Bot Battlr</h1>
+                <nav className="w-1/2">
+                    <button className="bg-blue-500 py-2 px-4 rounded ml-3 mr-3 hover:bg-blue-700 text-white font-bold"><Link to='/home'>Home</Link></button>
+                    <button className="bg-blue-500 py-2 px-4 rounded ml-3 mr-3 hover:bg-blue-700 text-white font-bold"><Link to='/bot-collection'>Bot Collection</Link></button>
+                    <button className="bg-blue-500 py-2 px-4 rounded ml-3 mr-3 hover:bg-blue-700 text-white font-bold"><Link to='/your-bot-army'>Your Bot Army</Link></button>
+                </nav>
+            </div>
             <Outlet></Outlet>
         </div>
      );
--- src/components/YourBotArmy.js
+++ src/components/YourBotArmy.js
@@ -1,5 +1,5 @@
+const YourBotArmy = ({yourBots, onDeleteBot}) => {
 
-const YourBotArmy = ({yourBots}) => {
     /*
         Add an individual bot to my army by clicking on it. The selected bot should render in this component, and the bot can only be enlisted ONCE. The bot does not disappear from the BotsCollection.
 
@@ -10,26 +10,42 @@ const YourBotArmy = ({yourBots}) => {
     //Loop over the bots to return the bots in YourBotArmy.
     const yourBotsList = yourBots.map((yourBot)=>{
         return(
-            <div key={yourBot.id} onClick={()=>onDeleteBot(yourBot)}>
-            <div key={yourBot.id}>
-                <img src={yourBot.avatar_url} alt={yourBot.name}/>
-                <h3>{yourBot.name}</h3>
-                <p>Class: {yourBot.bot_class}</p>
-                <p>Catch phrase: {yourBot.catchphrase}</p>
-                <p>Created at: {yourBot.created_at}</p>
-                <p>Updated at: {yourBot.updated_at}</p>
-                Health: <progress value={yourBot.health} max='100'/>
-                Damage: <progress value={yourBot.damage} max='100'/>
-                Armor: <progress value={yourBot.armor} max='100'/>
+            <div>
+                <div key={yourBot.id} className="max-w-sm rounded overflow-hidden shadow-lg p-5 m-5 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300">
+                    <img src={yourBot.avatar_url} alt={yourBot.name} className="w-ful"/>
+                    <h3 className="font-bold text-xl mb-2">{yourBot.name}</h3>
+                    <p className="text-gray-700 text-base">Class: {yourBot.bot_class}</p>
+                    <p>Catch phrase: {yourBot.catchphrase}</p>
+                    <p>Created at: {yourBot.created_at}</p>
+                    <p>Updated at: {yourBot.updated_at}</p>
+                    <div className="flex flex-col justify-center items-center">
+                        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-200 m-2">
+                        <div className="bg-blue-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${yourBot.health}%` }}>Health</div>
+                        </div>
+
+                        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-200 m-2">
+                        <div className="bg-red-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${yourBot.damage}%` }}>Damage</div>
+                        </div>
+
+                        <div className="w-full bg-gray-200 rounded-full dark:bg-gray-200 m-2">
+                        <div className="bg-green-500 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{ width: `${yourBot.armor}%` }}>Armor</div>
+                        </div>
+                    </div>
+                    
+                    <button className="text-white font-semibold bg-blue-500 rounded px-2 py-2 m-2 hover:bg-blue-700" onClick={()=>onDeleteBot(yourBot)}>Remove Bot From Collection</button>
+                </div>
             </div>
         )
     })
 
     return ( 
-        <div>
-            <h1>This is Your Bots List</h1>
-            {yourBotsList}
-        </div>
+        <>
+            <h1 className="text-xl font-bold mt-5 mb-5">This is Your Bots List</h1>
+            <div className="flex flex-wrap justify-center">
+                {yourBotsList}
+            </div>
+            
+        </>
      );
 }
  
